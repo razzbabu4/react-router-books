e9ef26d118a518851487159531d6118b73882e4a
Tareq <razzbabut@gmail.com>
2024-03-26 11:53:23 +0600

    import react icons and google images

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,23 +1,24 @@
 import PropTypes from 'prop-types';
+import { CiStar } from "react-icons/ci";
+
 const BookCard = ({ book }) => {
 
     const { image, tags, bookName, author, category, rating } = book
     return (
         <div>
             <div className="card w-full bg-base-100 shadow-xl p-6 border">
-                <figure><img className='h-60 w-full' src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="books" /></figure>
+                <figure className='bg-[#faf9f9]'><img className='h-80 py-10' src={image} alt="books" /></figure>
                 <div className="card-body px-0 pb-2">
-                    <ul className="flex gap-6 text-[#23BE0A] font-medium text-lg">
+                    <ul className="flex gap-6 text-[#23BE0A] font-medium text-lg pl-2">
                         {
                             tags.map((tag, idx) => <li key={idx}>{tag}</li>)
                         }
                     </ul>
                     <h2 className="card-title font-bold text-2xl">{bookName}</h2>
-                    <p className="font-medium">By: {author}</p>
+                    <p className="font-medium text-[#131313CC]">By : {author}</p>
                     <div className="divider"></div>
-                    <div className="flex justify-between">
-                        <span>{category}</span>
-                        <span>{rating}</span>
+                    <div className="flex justify-between font-medium">
+                        <span>{category}</span> <span className='flex items-center gap-2'>{rating} <CiStar className='text-xl' /></span>
                     </div>
                 </div>
             </div>
